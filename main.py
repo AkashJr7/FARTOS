8d9e3aec2875669a9db904d2ade8c5f1cf6fa42d
AkashJr7 <133139753+AkashJr7@users.noreply.github.com>
2025-04-06 22:05:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -359,8 +359,8 @@ def logout():
     return redirect('/login')
 
 @app.route('/')
-def indexx():
-    return render_template('signup.html')
+def home():
+    return render_template('index.html')
 
 
 	
@@ -465,5 +465,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
     
