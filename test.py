91dead2a78e3e479192b0337ff6e10a9e6733672
AkashJr7 <133139753+AkashJr7@users.noreply.github.com>
2025-04-07 16:36:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 
 from tkinter_scroll import *
 from tkinter import *
-from Pillow import Image
-from Pillow import ImageTk
+from PIL import Image
+from PIL import ImageTk
 import cv2, threading, os, time
 from threading import Thread
 from os import listdir
