f3fec667c63affe355b73af65f0ab8ce3f69c9a8
AkashJr7 <133139753+AkashJr7@users.noreply.github.com>
2025-04-07 16:25:00 +0530

    Update tryOn.py

--- tryOn.py
+++ tryOn.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from PIL import Image
-from PIL import ImageTk
+from Pillow import Image
+from Pillow import ImageTk
 import cv2, threading, os, time
 from threading import Thread
 from os import listdir
